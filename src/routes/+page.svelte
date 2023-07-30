9b34f837a38b3c77319fb76a11f120a4e86cc5f2
Kasama Thongsawang <kasama.t@catnip.local>
2023-07-30 22:33:48 +0700

    feat: random menu

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import _ from 'lodash';
+	const foods: string[] = ['pad kapao', 'khai jeaw moo sub', 'kaprao kung', 'pad thai', 'hoy tod'];
+
+	let menu: string;
+
+	function setNewMenu() {
+		let newMenu: string;
+		let count: number = 0;
+		do {
+			newMenu = _.sample(foods) || '';
+			count++;
+		} while (_.isEqual(newMenu, menu) && count < 5);
+		menu = newMenu;
+	}
+
+	setNewMenu();
+</script>
+
+<h1>{menu}</h1>
+<button on:click={setNewMenu}>new menu</button>
