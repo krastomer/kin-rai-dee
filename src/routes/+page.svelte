0a1f6627d2b73230db4f9a9bb16ba00c47c1e4ed
krastomer <fitmerdox@outlook.com>
2023-07-31 18:55:27 +0700

    feat: add new menu

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,7 +11,7 @@
 		let newMenu: string;
 		let count: number = 0;
 		do {
-			newMenu = _.sample(foods) || '';
+			newMenu = _.sample(foods)?.name || '';
 			count++;
 		} while (_.isEqual(newMenu, menu) && count < 5);
 		if (menu) {
@@ -28,15 +28,37 @@
 	setNewMenu();
 </script>
 
-<section class="fixed h-screen w-screen text-center place-items-center">
-	<h1>{menu}</h1>
-	<div>
-		<Button on:click={setNewMenu}>new menu</Button>
+<div class="fixed flex flex-col h-screen w-screen items-center justify-center">
+	<p class="text-9xl">{menu}</p>
+	<div class="mt-10">
+		<Button color="alternative" on:click={setNewMenu}>new menu</Button>
 		<Button on:click={resetRecentMenu}>reset</Button>
 	</div>
-	<ul class="text-left">
-		{#each recentPick as name, i}
-			<h2>{i + 1}: {name}</h2>
-		{/each}
-	</ul>
-</section>
+</div>
+
+<div class="absolute bottom-0 left-0 w-screen">
+	<footer class="bg-white rounded-lg shadow m-4 dark:bg-gray-800 relative">
+		<div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+			<span class="text-sm text-gray-500 sm:text-center dark:text-gray-400"
+				>© 2023 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights
+				Reserved.
+			</span>
+			<ul
+				class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0"
+			>
+				<li>
+					<a href="#" class="mr-4 hover:underline md:mr-6">About</a>
+				</li>
+				<li>
+					<a href="#" class="mr-4 hover:underline md:mr-6">Privacy Policy</a>
+				</li>
+				<li>
+					<a href="#" class="mr-4 hover:underline md:mr-6">Licensing</a>
+				</li>
+				<li>
+					<a href="#" class="hover:underline">Contact</a>
+				</li>
+			</ul>
+		</div>
+	</footer>
+</div>
